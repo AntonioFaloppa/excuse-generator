fafacdb680f1e1eaaa841d39933236a7846df706
AntonioFaloppa <antoniofaloppa@hotmail.com>
2022-10-05 08:41:54 -0400

    update: add gitignore and minor changes ✅

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ let time = ['before the class', 'during my lunch', 'riding in the car', 'while d
 const sentenceOfEveryList = (array) => {
     let position = Math.floor(Math.random()*array.length);
     return position;
-}
+};
 const sentenceGenerator = (who, action, what, time) => { 
     let sentence = `${who[sentenceOfEveryList(who)]} ${action[sentenceOfEveryList(action)]} ${what[sentenceOfEveryList(what)]} ${time[sentenceOfEveryList(time)]}`;
     return sentence;
-}
+};
 window.onload = () => {
     let excuse = document.querySelector("#excuse");
     excuse.innerHTML = sentenceGenerator(who, action, what, time);
-}
+};
